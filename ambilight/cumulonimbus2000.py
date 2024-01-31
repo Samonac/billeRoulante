a4e8b0ca9be3ed969c67b118acf247cdbac4c1ee
Samonac <samonac@hotmail.com>
2024-01-31 16:41:23 +0100

    adding glow and weather dynamics

--- ambilight/cumulonimbus2000.py
+++ ambilight/cumulonimbus2000.py
@@ -158,6 +158,20 @@ async def getweather(saveJson=False):
         with open('data/weather/{}.json'.format(fileName), "w") as outfile:
             outfile.write('{}'.format(jsonOutput))
 
+    return jsonOutput
+
+def getLatestData():
+    jsonArray = glob.glob('data/weather/*.json')
+    fileName = jsonArray[0]
+
+    weatherJson =json.load(fileName)
+    jsonArray = glob.glob('data/ratp/*.json')
+    fileName = jsonArray[0]
+
+    ratpJson =json.load(fileName)
+    # with open('data/weather/{}.json'.format(fileName), "w") as weatherFile:
+    #     jsonOutput = weatherFile.read()
+    return [weatherJson, ratpJson]
 
 # LED strip configuration:
 LED_COUNT = 240        # Number of LED pixels.
@@ -175,6 +189,178 @@ LED_INVERT = False    # True to invert the signal (when using NPN transistor lev
 LED_CHANNEL = 0      # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_CHANNEL_1 = 1      # set to '1' for GPIOs 13, 19, 41, 45 or 53
 LED_CHANNEL_2 = 0      # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+rgbLightDict = {}
+### https://www.schemecolor.com/sky-weather.php
+# VIVID CERULEAN
+rgbLightDict['dark_blue'] = [13, 157, 227]
+# CAPRI
+rgbLightDict['middle_blue'] = [1, 191, 255]
+# VIVID SKY BLUE
+rgbLightDict['light_blue'] = [0, 204, 255]
+# ANTI-FLASH WHITE
+rgbLightDict['light_grey'] = [241, 241, 241]
+# WELDON BLUE
+rgbLightDict['middle_grey'] = [127, 155, 166]
+# STORMCLOUD
+rgbLightDict['dark_grey'] = [78, 105, 105]
+
+### sunset levels : https://www.color-hex.com/color-palette/1040079
+rgbLightDict['sunset1'] = [242,176,53] #yellow / orange
+rgbLightDict['sunset2'] = [241,157,0]
+rgbLightDict['sunset3'] = [242,143,22]
+rgbLightDict['sunset4'] = [212,140,5]
+rgbLightDict['sunset5'] = [242,101,19]
+rgbLightDict['sunset6'] = [210,105,0]
+rgbLightDict['sunset7'] = [217,59,24]
+rgbLightDict['sunset8'] = [195,86,3]
+rgbLightDict['sunset9'] = [152,66,0]
+rgbLightDict['sunset10'] = [217,30,30] #red
+
+weatherDict = {}
+weatherDict['SUNNY'] = 'light_blue'  # 113
+weatherDict['PARTLY_CLOUDY'] = 'light_grey'  #116
+weatherDict['CLOUDY'] = 'middle_grey'  # 119
+weatherDict['VERY_CLOUDY'] = 'dark_grey'  # 122
+weatherDict['FOG'] = 'light_grey'  #143
+weatherDict['LIGHT_SHOWERS'] = 'light_grey'  #176
+weatherDict['LIGHT_SLEET_SHOWERS'] = 'light_grey'  #179
+weatherDict['LIGHT_SLEET'] = 'light_grey'  #182
+weatherDict['THUNDERY_SHOWERS'] = 'dark_grey'  # 200
+weatherDict['LIGHT_SNOW'] = 'light_grey'  #227
+weatherDict['HEAVY_SNOW'] = 'dark_grey'  # 230
+weatherDict['LIGHT_RAIN'] = 'light_blue'  #266
+weatherDict['HEAVY_SHOWERS'] = 'dark_grey'  # 299
+weatherDict['HEAVY_RAIN'] = 'dark_grey'  # 302
+weatherDict['LIGHT_SNOW_SHOWERS'] = 'middle_grey'  # 323
+weatherDict['HEAVY_SNOW_SHOWERS'] = 'dark_grey'  # 335
+weatherDict['THUNDERY_HEAVY_RAIN'] = 'middle_grey'  # 389
+weatherDict['THUNDERY_SNOW_SHOWERS'] = 'dark_grey'  # 392
+
+#
+# weatherToRGBdict = {  # description/kind
+#     'Overcast': {
+#         'Very Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#     },
+#     'Light drizzle': {
+#             'Light Rain': [rgbLight_grey, rgbLight_darkGrey],
+#             'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#             'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#         },
+#     'Mist': {
+#             'Fog': [rgbLight_grey, rgbLight_darkGrey],
+#             'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#             'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#         },
+#     'Patchy rain possible': {
+#         'Light Showers': [rgbLight_grey, rgbLight_darkGrey],
+#         'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#     },
+#     'Cloudy': {
+#         'Light Rain': [rgbLight_grey, rgbLight_darkGrey],
+#         'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#     },
+#     'Sunny': {
+#         'Sunny': [rgbLight_grey, rgbLight_darkGrey],
+#         'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#     },
+#     'Partly cloudy': {
+#         'Partly Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#     },
+#     'Clear': {
+#         'Sunny': [rgbLight_grey, rgbLight_darkGrey],
+#         'Cloudy': [rgbLight_grey, rgbLight_darkGrey],
+#         'Clear': [rgbLight_grey, rgbLight_darkGrey]
+#     }
+# }
+
+def startWeatherMode(stripArray, weatherJson):
+    [strip240, strip120] = stripArray
+    print('In startWeatherMode with weatherJson[now] : ', weatherJson['now'])
+    # nowRgb = weatherToRGBdict[weatherJson['now']['description']['kind']]
+    nowRgb = rgbLightDict[weatherDict[weatherJson['now']['description'].upper()]]
+
+    currentTime = datetime.datetime.now()
+    currentHour = currentTime.hour
+    currentMin = currentTime.minute
+    sunsetTime = '18:00:00'
+    sunriseTime = '08:00:00'
+    for keyTemp in weatherJson.keys():
+        try:
+            sunsetTime = weatherJson[keyTemp]['astronomy']['sun_set']
+            sunriseTime = weatherJson[keyTemp]['astronomy']['sun_rise']
+        except KeyError as err:
+            print('did not find Astronomy in ', keyTemp)
+    # currentTime = '{}'.format(currentTime).replace(' ', '_').replace(':', '-').replace('.', '_')
+
+    currentSunsetLevel = 'sunset{}'.format(max(1, int(float(currentHour) / 2.4)))
+    try:
+        sunsetRgb = rgbLightDict[currentSunsetLevel]
+    except KeyError as err:
+        print(err)
+        sunsetRgb = rgbLightDict['sunset10']
+    rgb1 = nowRgb
+    rgb2 = nowRgb
+    blackRgb = [0, 0, 0]
+    print('CurrentSunsetLevel : ', currentSunsetLevel)
+
+    if currentHour <= int(sunriseTime.split(':')[0]) and currentMin <= int(sunriseTime.split(':')[1]):
+        print('Sun has not yet risen : staying dark (maybe stars every so often ?)')
+        rgb1 = blackRgb
+        rgb2 = blackRgb
+    if currentHour >= int(sunriseTime.split(':')[0]) and currentMin >= int(sunriseTime.split(':')[1]):
+        print('Sun has risen, we are after sunriseTime : ', sunriseTime)
+        print('rgb 1 and 2 will be set to nowRgb = ', nowRgb)
+        rgb1 = nowRgb
+        rgb2 = nowRgb
+    if currentHour > int(sunsetTime.split(':')[0]) or (currentHour >= int(sunsetTime.split(':')[0]) and currentMin >= int(sunsetTime.split(':')[1])):
+        print('Sun has set, we are after sunsetTime : ', sunsetTime)
+        rgb2 = sunsetRgb
+        print('rgb2 will be set to sunsetRgb = ', sunsetRgb)
+    if currentHour >= 23:
+        print('Need to get ready for bed')
+        rgb1 = sunsetRgb
+        print('rgb1 will (also) be set to sunsetRgb = ', rgb1)
+
+    [R1, G1, B1] = rgb1
+    [R2, G2, B2] = rgb2
+    colorArray = [R1, G1, B1, R2, G2, B2]
+
+    glow(stripArray, colorArray)
+
+
+def glow(stripArray, colorArray, wait_ms=1000, percent=5, loop=10):
+    print('In glow with colorArray : ', colorArray)
+    print('In glow with percent : ', percent)
+    print('In glow with loop : ', loop)
+    [strip240, strip120] = stripArray
+    [R1, G1, B1, R2, G2, B2] = colorArray
+
+    colorTemp1 = Color(int(float(R1)*float(percent)/100.0), int(float(G1)*float(percent)/100.0), int(float(B1)*float(percent)/100.0))
+    colorTemp2 = Color(int(float(R2)*float(percent)/100.0), int(float(G2)*float(percent)/100.0), int(float(B2)*float(percent)/100.0))
+    # colorTemp2 = Color(R2, G2, B2)
+    # inverseColorTemp = Color(255-R, G, B)
+    for i in range(strip120.numPixels()):
+        strip120.setPixelColor(i, colorTemp1)
+
+    for i in range(strip240.numPixels()):
+        strip240.setPixelColor(i, colorTemp2)
+
+    strip120.show()
+    strip240.show()
+    if (wait_ms > 0): time.sleep(wait_ms / 1000.0)
+
+    if loop>0:
+        glow(stripArray, colorArray, wait_ms=wait_ms, percent=max(0, 100-10*loop), loop=int(loop-1))
+
+
 
 def doubleColorWipe(stripArray, colorArray, wait_ms=50):
     [strip240, strip120] = stripArray
@@ -302,7 +488,7 @@ if __name__ == '__main__':
     try:
 
         while True:
-            print('Color wipe animations.')
+            print('Boot : Color wipe animations.')
 
             # colorWipe(strip240, Color(0, 255, 0))  # Green wipe
             # colorWipe(strip240, Color(0, 0, 255))  # Blue wipe
@@ -351,8 +537,14 @@ if __name__ == '__main__':
             # theaterChaseRainbow(strip120)
             # theaterChaseRainbow(strip240)
 
+            #  Waiting for instructions ? Automatically start weatherMode ?
+            [weatherJson, ratpJson] = getLatestData()
+            startWeatherMode([strip240, strip120], weatherJson)
+            # startRatpMode(ratpJson)
+
+
     except KeyboardInterrupt:
         colorWipe(strip120, Color(0, 0, 0), 10)
         colorWipe(strip240, Color(0, 0, 0), 10)
 
-print('Script has ended. Good night !')
+print('Cumulonimbus2000 script has ended. Good night !')
